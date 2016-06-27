deb6e4ed45d16db25b7a7175561eadf75994b44b
Jonathan Nicolas & Sneha Narasimhan <pair+jonathanNicolas.snehabn@devbootcamp.com>
2016-06-26 17:09:37 -0700

    Initialize causes controller

--- app/controllers/causes_controller.rb
+++ app/controllers/causes_controller.rb
@@ -1,2 +1,32 @@
 class CausesController < ApplicationController
+  def index
+    @causes = Cause.all
+  end
+
+  def show
+    @cause = Cause.find_by(id: params[:id])
+    @organizations = Cause.where(id: params[:id], causable_type: 'Organization').map do |cause| cause.causable end
+    @users = Cause.where(id: params[:id], causable_type: 'User').map do |cause| cause.causable end
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def cause_params
+    params.permit(:name, :causable_id, :causable_type)
+  end
+
 end
