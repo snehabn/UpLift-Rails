3e8fedb271130a73560108e7686714d226cf02c5
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-06-26 18:01:09 -0700

    Add controllers for events and events_users.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,11 +20,12 @@ class EventsController < ApplicationController
 
 	end
 
-	def edit
-	end
+	# def edit
+	# end
 
-	def update
-	end
+	# def update
+		
+	# end
 
 	def destroy
 		@event = Event.find(params[:id])
